40a1bb418dc348991d9aa8508358dc1078854747
Tisherd <DYXA97@yandex.ru>
2021-07-10 12:08:46 +0800

    add venv change time.sleep 8 to 30

--- test_items.py
+++ test_items.py
@@ -4,5 +4,5 @@ import time
 def test_checking_button_ShopCard(browser):
     link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
     browser.get(link)
-    time.sleep(8)
+    time.sleep(30)
     assert browser.find_element_by_css_selector(".btn.btn-lg.btn-primary.btn-add-to-basket")
