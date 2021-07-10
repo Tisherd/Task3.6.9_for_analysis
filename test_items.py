fa3b2aa1019bc874c7696a3f710efd2f3678d676
Tisherd <DYXA97@yandex.ru>
2021-07-10 12:07:42 +0800

    add venv

---
+++ test_items.py
@@ -0,0 +1,8 @@
+import time
+
+
+def test_checking_button_ShopCard(browser):
+    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+    browser.get(link)
+    time.sleep(8)
+    assert browser.find_element_by_css_selector(".btn.btn-lg.btn-primary.btn-add-to-basket")
